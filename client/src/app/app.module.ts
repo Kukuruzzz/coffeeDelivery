e96aa1edf2c316fbb0369d1cd0de581e8e36f121
Kukuruzzz <kukuruzzzmail@gmail.com>
2017-08-14 13:53:05 +0300

    Client auth basket nav products init commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { NavComponent } from './nav/nav.component';
+import { BasketComponent } from './basket/basket.component';
+import { AuthComponent } from './auth/auth.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NavComponent,
+    BasketComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
